091fee6b2d4d9939c8b2c782eeb81d7b8e4a5c7e
nCoLlInS24 <natankcollins@gmail.com>
2023-04-21 13:41:38 +0000

    changes

--- App/views/index.py
+++ App/views/index.py
@@ -13,7 +13,7 @@ def login():
 def init():
     db.drop_all()
     db.create_all()
-    create_user('bob', 'bobpass')
+    create_user('bob', 'bobpass','bob@email.com')
     return jsonify(message='db initialized!')
 
 @index_views.route('/health', methods=['GET'])
