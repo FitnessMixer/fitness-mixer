936ce6cc0d1993a20bfa40118f7d13a55d3a6890
nCoLlInS24 <natankcollins@gmail.com>
2023-04-22 17:34:31 +0000

    try check password

--- App/models/user.py
+++ App/models/user.py
@@ -1,4 +1,4 @@
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash, check_password
 from flask_login import UserMixin
 from App.database import db
 
