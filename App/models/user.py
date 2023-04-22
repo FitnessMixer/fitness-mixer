0e98dd3ab6e206ec9c4c1d12e0b099635946aa89
nCoLlInS24 <natankcollins@gmail.com>
2023-04-22 17:53:48 +0000

    fixed maybe?

--- App/models/user.py
+++ App/models/user.py
@@ -1,4 +1,4 @@
-from werkzeug.security import check_password_hash, generate_password_hash, check_password
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 from App.database import db
 
@@ -9,24 +9,22 @@ class User(db.Model, UserMixin):
     email =  db.Column(db.String, nullable=False, unique=True)
     # user_routine = db.relationship('user_routine', backref='user', lazy=True, cascade="all, delete-orphan")
 
-
-def __init__(self, username, password,email):
+    def __init__(self, username, password,email):
         self.username = username
         self.set_password(password)
         self.email=email
         self.id=id(self)
 
-def get_json(self):
-    return{
-        'id': self.id,
-        'username': self.username
+    def get_json(self):
+        return{
+            'id': self.id,
+            'username': self.username
         }
-
-def set_password(self, password):
-    """Create hashed password."""
-    self.password = generate_password_hash(password, method='sha256')
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(password, method='sha256')
     
-def check_password(self, password):
-    """Check hashed password."""
-    return check_password_hash(self.password, password)
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.password, password)
 
