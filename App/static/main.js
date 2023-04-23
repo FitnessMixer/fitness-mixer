1ee37557b1b57eb84aa9cc297e768f8e4b1d9a5c
JustaSingular <mitchellpeterdaniel@gmail.com>
2023-04-23 18:45:51 +0000

    edited login, signup, and fixed backgroud for home , added html page with the timer

--- App/static/main.js
+++ App/static/main.js
@@ -20,3 +20,43 @@ async function main(){
 }
 
 main();
+
+var timerInterval;
+      var timeLeft = 0;
+      
+      function startTimer() {
+        var minutes = document.getElementById("minutes").value;
+        var seconds = document.getElementById("seconds").value;
+        timeLeft = parseInt(minutes) * 60 + parseInt(seconds);
+        
+        timerInterval = setInterval(function() {
+          var minutesLeft = Math.floor(timeLeft / 60);
+          var secondsLeft = timeLeft % 60;
+          
+          document.getElementById("timer").innerHTML = minutesLeft + ":" + (secondsLeft < 10 ? "0" : "") + secondsLeft;
+          
+          if (timeLeft == 0) {
+            clearInterval(timerInterval);
+            document.getElementById("timer").innerHTML = "Done!";
+            var audio = new Audio('http://soundbible.com/grab.php?id=1705&type=wav');
+            audio.play();
+          } else {
+            timeLeft--;
+          }
+        }, 1000);
+      }
+      
+      function pauseTimer() {
+        clearInterval(timerInterval);
+      }
+      
+      function resetTimer() {
+        clearInterval(timerInterval);
+        document.getElementById("minutes").value = "0";
+        document.getElementById("seconds").value = "0";
+        document.getElementById("timer").innerHTML = "0:00";
+      }
+      
+      function resumeTimer() {
+        startTimer();
+      }
