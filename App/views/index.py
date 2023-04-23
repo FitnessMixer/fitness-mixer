1e5f452fcb43ac37d3dcc67c103af3c837e3ec65
moniquebritto21 <82354847+moniquebritto21@users.noreply.github.com>
2023-04-23 21:42:13 +0000

    trying something

--- App/views/index.py
+++ App/views/index.py
@@ -10,6 +10,26 @@ from App.controllers.user import create_user,editEmail,check_password
 
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
+def getExercises():
+    muscle = 'abdominals'
+    i=0
+    muscles=['abdominals','abductors','adductors','biceps','calves','chest','forearms','glutes','hamstrings','lats','lower_back','middle_back','neck','quadriceps','traps','triceps']
+    for m in muscles:
+        api_url = 'https://api.api-ninjas.com/v1/exercises?muscle={}'.format(muscles[i])
+        response = requests.get(api_url, headers={'X-Api-Key': 'NwmKx1s20Ive3BSqoYMvmw==zbTgNEmqqVzTlGT4'})
+        i+=1   
+        if response.status_code == requests.codes.ok:
+            response_json = json.loads(response.text)
+            # Convert the JSON string to a Python list
+            exercises = response_json
+            for x in exercises:
+                exercise=Exercise(name=x["name"],muscle=x["muscle"],category=x["type"],equipment=x['equipment'],difficulty=x["difficulty"],instructions=x["instructions"])
+                db.session.add(exercise)
+                db.session.commit()
+                #print("Exercises added")
+        else:
+            print("Error:")
+
 
 @index_views.route('/', methods=['GET'])
 def login():
@@ -19,6 +39,7 @@ def init():
     db.drop_all()
     db.create_all()
     create_user('bob', 'bobpass','bob@email.com')
+    getExercises();
     return jsonify(message='db initialized!')
 
 
